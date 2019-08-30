baea84b0c3c74fc1d6036656403020175ce46385
ImenNrnaes <naes.imen1@gmail.com>
2019-08-30 11:57:15 +0100

    Consume crud from webApi in potail

--- Admin/Controllers/PaymentController.cs
+++ Admin/Controllers/PaymentController.cs
@@ -1,9 +1,6 @@
 ﻿using Admin.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Admin.Controllers
@@ -20,5 +17,75 @@ namespace Admin.Controllers
             paymentlist = response.Content.ReadAsAsync<IEnumerable<Payment>>().Result;
             return View(paymentlist);
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Payment payment)
+        {
+            var addpayment = Helper.Helper.WebApiClient.PostAsJsonAsync<Payment>("Payment",payment);
+            addpayment.Wait();
+            var result = addpayment.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+
+            }
+            ModelState.AddModelError(string.Empty, "Something wrong!! please try again...");
+            return View(payment);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Payment payment = null;
+            var responseTask = Helper.Helper.WebApiClient.GetAsync("payment/" + id.ToString());
+            responseTask.Wait();
+
+            var result = responseTask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                var readTask = result.Content.ReadAsAsync<Payment>();
+                readTask.Wait();
+
+                payment = readTask.Result;
+            }
+            
+            return View(payment);
+        }
+        [HttpPost]
+        public ActionResult Edit(Payment payment)
+        {
+            //HTTP Put
+            var edit = Helper.Helper.WebApiClient.PutAsJsonAsync("payment/"+ payment.ID, payment);
+            edit.Wait();
+            var result = edit.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(payment);
+
+        }
+
+        public void Delete(int id)
+        {
+            //HTTP Delete
+            var deletetask = Helper.Helper.WebApiClient.DeleteAsync("Payment/" +id.ToString());
+            deletetask.Wait();
+
+            var result = deletetask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                RedirectToAction("Index");
+            }
+
+
+            RedirectToAction("Index");
+        }
+       
     }
 }
