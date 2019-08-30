36f76bfb6d2831b1f24e6d7432f4296c7b8f8e60
ImenNrnaes <naes.imen1@gmail.com>
2019-08-30 13:30:24 +0100

    Add Crud product-- Admin

---
+++ Admin/Controllers/ProductController.cs
@@ -0,0 +1,82 @@
+﻿using Admin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            IEnumerable<Product> Listproducts;
+            HttpResponseMessage response = Helper.Helper.WebApiClient.GetAsync("Product").Result;
+            Listproducts = response.Content.ReadAsAsync<IEnumerable<Product>>().Result;
+            return View(Listproducts);
+        }
+
+        public ActionResult Create()
+        {          
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            var AddTask = Helper.Helper.WebApiClient.PostAsJsonAsync<Product>("Product", product);
+            AddTask.Wait();
+            var result = AddTask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        public ActionResult Edit(Guid id)
+        {
+            Product prod = null;
+            var product = Helper.Helper.WebApiClient.GetAsync("Product/"+id.ToString());
+            product.Wait();
+
+            var result = product.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                var prdetoedit = result.Content.ReadAsAsync<Product>();
+                prdetoedit.Wait();
+                prod = prdetoedit.Result;
+            }
+            return View(prod);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            var editTask = Helper.Helper.WebApiClient.PutAsJsonAsync<Product>("Product/"+ product.ID, product);
+            editTask.Wait();
+            var result = editTask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+
+        public ActionResult Delete(Guid id)
+        {
+            var deletetask = Helper.Helper.WebApiClient.DeleteAsync("Product/" + id.ToString());
+            
+            var result = deletetask.Result;
+            if (result.IsSuccessStatusCode)
+            {   
+                return RedirectToAction("Index");
+            }
+            return View("Delete"); 
+        }
+    }
+}
---
+++ Admin/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Admin.Models
+{
+    public class Product
+    {
+        [Key]
+        public Guid ID { get; set; }
+        public Guid UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public bool IsDeleted { get; set; }
+        public int SuggestedCount { get; set; }
+        public string Value1 { get; set; }
+        public string Label { get; set; }
+        public string LabelEn { get; set; }
+        public decimal? FromPrice { get; set; }
+        public decimal? Discount { get; set; }
+        public DateTime? ExpiryDate { get; set; }
+        public Guid? CitiesID { get; set; }
+        public DateTime? EndDate { get; set; }
+        public bool PreOrder { get; set; }
+        public string Code { get; set; }
+        public Guid? ProductSmallImagesID { get; set; }
+        public DateTime? StartDate { get; set; }
+    }
+}
